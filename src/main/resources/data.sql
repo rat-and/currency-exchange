016225246527da8995e2815c922849bbcc4a2908
Andre DESKTOP-3HEJK6B <jatar00@onet.eu>
2021-06-02 17:19:08 +0200

    Breakout controller added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+values (1234, 'USD', 'PLN', 4, '');
+
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+values (1235, 'AUD', 'PLN', 3, '');
+
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+values (1236, 'EUR', 'PLN', 4, '');
