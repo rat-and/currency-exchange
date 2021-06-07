b7f48c66571279893e38091dd948a65b051bb017
Andre DESKTOP-3HEJK6B <jatar00@onet.eu>
2021-06-07 12:41:18 +0200

    Added zipkin connection for currency exchange service

--- src/main/java/edu/springboot/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/edu/springboot/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package edu.springboot.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
     @Autowired
     private CurrencyExchangeRepository repository;
@@ -19,6 +23,8 @@ public class CurrencyExchangeController {
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to) {
+    	
+    	logger.info("retrieveExchangeValue method called with pramaters: {}, {}", from, to);
 
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
         if (currencyExchange == null) {
